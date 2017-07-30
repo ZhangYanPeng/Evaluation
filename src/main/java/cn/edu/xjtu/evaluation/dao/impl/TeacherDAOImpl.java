1c4dc42a812a74ecd66d2350d6dcec61b623f07b
ZhangYanPeng <cn.yanpengzhang@qq.com>
2017-07-31 07:33:45 +0800

    nothing

---
+++ src/main/java/cn/edu/xjtu/evaluation/dao/impl/TeacherDAOImpl.java
@@ -0,0 +1,10 @@
+package cn.edu.xjtu.evaluation.dao.impl;
+
+import org.springframework.stereotype.Repository;
+
+import cn.edu.xjtu.evaluation.entity.Teacher;
+
+@Repository
+public class TeacherDAOImpl extends BaseDAOImpl<Teacher, Long>{
+
+}
