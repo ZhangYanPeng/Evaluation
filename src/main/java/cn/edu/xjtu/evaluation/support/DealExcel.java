4384a09e548370e99cdaf3f9afcd77ad37b62d42
ZhangYanPeng <cn.yanpengzhang@qq.com>
2017-08-11 14:54:18 +0800

    POI in

--- src/main/java/cn/edu/xjtu/evaluation/support/DealExcel.java
+++ src/main/java/cn/edu/xjtu/evaluation/support/DealExcel.java
@@ -1,7 +1,9 @@
 package cn.edu.xjtu.evaluation.support;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
 import org.apache.poi.ss.usermodel.Cell;
@@ -13,41 +15,33 @@ import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 public class DealExcel {
 	public static void main(String[] args) {
 		try {
-			loadInStudentInfo((long) 1,"F:\\Workspace\\.metadata\\.plugins\\org.eclipse.wst.server.core\\tmp0\\wtpwebapps\\Evaluation\\upload\\stu_list\\2110.xlsx");
-		} catch (BiffException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+			loadInStudentInfo((long) 1,
+					"F:\\Workspace\\.metadata\\.plugins\\org.eclipse.wst.server.core\\tmp0\\wtpwebapps\\Evaluation\\upload\\stu_list\\2110.xlsx");
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+
 	}
 
-	public static void loadInStudentInfo(Long cid, String filepath) throws BiffException, IOException {
+	public static void loadInStudentInfo(Long cid, String filepath) throws IOException {
 		// TODO Auto-generated method stub
-		File xlsFile = new File(filepath);
-		// 获得工作簿对象
-		XSSFWorkbook  workbook = Workbook.getWorkbook(xlsFile);
-		// 获得所有工作表
-		Sheet[] sheets = workbook.getSheets();
-		// 遍历工作表
-		if (sheets != null) {
-			for (Sheet sheet : sheets) {
-				// 获得行数
-				int rows = sheet.getRows();
-				// 获得列数
-				int cols = sheet.getColumns();
-				// 读取数据
-				for (int row = 0; row < rows; row++) {
-					for (int col = 0; col < cols; col++) {
-						System.out.printf("%10s", sheet.getCell(col, row).getContents());
-					}
-					System.out.println();
-				}
-			}
+		InputStream stream = new FileInputStream(filepath);
+		Workbook wb = null;
+		if (filepath.split(".")[1].equals("xls")) {
+			wb = new HSSFWorkbook(stream);
+		} else if (filepath.split(".")[1].equals("xlsx")) {
+			wb = new XSSFWorkbook(stream);
+		} else {
+			System.out.println("您输入的excel格式不正确");
 		}
-		workbook.close();
-
+		Sheet sheet1 = wb.getSheetAt(0);
+		for (Row row : sheet1) {
+			for (Cell cell : row) {
+				System.out.print(cell.getStringCellValue() + "  ");
+			}
+			System.out.println();
+		}
 	}
 
 }
