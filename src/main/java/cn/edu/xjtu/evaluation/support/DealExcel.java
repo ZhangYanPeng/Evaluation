a79dd3aa19871f819556f70ced32dc1e318c7c66
ZhangYanPeng <cn.yanpengzhang@qq.com>
2017-08-12 10:41:12 +0800

    nothing

--- src/main/java/cn/edu/xjtu/evaluation/support/DealExcel.java
+++ src/main/java/cn/edu/xjtu/evaluation/support/DealExcel.java
@@ -10,7 +10,6 @@ import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
-import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 public class DealExcel {
 	public static void main(String[] args) {
