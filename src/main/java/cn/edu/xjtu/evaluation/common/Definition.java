abcec94429883db9b066a25c1055a95d14179d6c
ZhangYanPeng <cn.yanpengzhang@qq.com>
2018-09-26 19:04:13 +0800

    tmp

---
+++ src/main/java/cn/edu/xjtu/evaluation/common/Definition.java
@@ -0,0 +1,7 @@
+package cn.edu.xjtu.evaluation.common;
+
+public class Definition {
+
+	public static enum QUESTION_LEVEL { easy, normal, hard, difficult};
+	public static enum QUESTION_TYPE { presentation, grammar, comprehension, details, inference };
+}
